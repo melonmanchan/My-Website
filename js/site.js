34fb3711b3e1b7d18983ba655002b46e0c0d63a6
Matti Jokitulppo <g8049@student.jamk.fi>
2015-07-11 22:42:52 +0300

    Add some content to about me, add modal on contact form submit

--- js/site.js
+++ js/site.js
@@ -2,4 +2,16 @@
 
 $(document).ready(function () {
     $(".button-collapse").sideNav();
+
+    $("form").submit(function (event) {
+       event.preventDefault();
+       var msg      =  $("#message").val();
+       var mail     = $("#email").val();
+       var subject  = $("#subject").val();
+
+        console.log(msg);
+        console.log(mail);
+        console.log(subject)
+        $('#modal1').openModal();
+    });
 });
