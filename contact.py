45d0b5437602c40aa409e9a4e26c9390e34b3a9c
Matti Jokitulppo <g8049@student.jamk.fi>
2015-07-12 12:47:44 +0300

    Working contact.py

--- contact.py
+++ contact.py
@@ -1,12 +1,27 @@
+# Simple mail server utilizing mailgun.net API
 from flask import Flask
+from flask import request
+import requests
 app = Flask(__name__)
 
 @app.route("/sendmail", methods= ['POST'])
 def hello():
-    return "Hello World!"
+    email    = request.form.get('email')
+    msg      = request.form.get('message')
+    subject  = request.form.get('subject')
+    send_mail(email, msg, subject)
+    return 'OK'
+
+
+def send_mail(email, message, subject):
+        return requests.post(
+            "https://api.mailgun.net/v3/sandbox12e603e0bce5426bbe66f14c68530ae3$
+            auth=("api", "xxxx"),
+            data={"from": email,
+                  "to": ["melonmanchan@gmail.com"],
+                  "subject": subject,
+                  "text": message})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
-
-
+    app.run(host='0.0.0.0')
 
