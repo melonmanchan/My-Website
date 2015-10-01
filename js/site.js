289842b24abe4817b8ea282b82c2847a1ec930c4
Matti Jokitulppo <melonmanchan@gmail.com>
2015-10-01 17:16:58 +0300

    Remove test alert

--- js/site.js
+++ js/site.js
@@ -1,7 +1,6 @@
 // Here be the site javascript!
 
 $(document).ready(function () {
-    alert("hello");
     $(".button-collapse").sideNav();
     $('.scrollspy').scrollSpy();
     $("form").submit(function (event) {
